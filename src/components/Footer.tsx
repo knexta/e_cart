614fb56f138a1b52754a51f67dace634991e5aec
Megaarasan <megaarasan2000@gmail.com>
2022-04-29 19:33:43 +0530

    updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,15 +9,15 @@ const Footer: React.FC = () => {
   return (
     <div className="bg-black">
       <div className="flex flex-auto flex-wrap xl:justify-center  p-[15px] w-[100%] ">
-        <div className="card-1  md:w-[100%] lg:w-[50%] xl:w-[23%] p-[15px] ">
-          <div>
+        <div className="  md:w-[100%] lg:w-[50%] xl:w-[60%] p-[15px] flex flex-row flex-wrap">
+          <div className="xl:w-[50%] md:w-[100%]">
             <img src={logo} alt="" className="image h-[100px] w-[100px] " />
+            <div className="text-2xl text-white p-[15px]">
+              <p>Got a question ? Call us 24/7</p>
+              <h4 className="text-red-500 text-3xl font-bold">1400 765 908</h4>
+            </div>
           </div>
-          <div className="text-2xl text-white p-[15px]">
-            <p>Got a question ? Call us 24/7</p>
-            <h4 className="text-red-500 text-3xl font-bold">1400 765 908</h4>
-          </div>
-          <div className="address">
+          <div className="address xl:w-[50%] md:w-[100%]">
             <h2 className="text-xl text-white pb-[15px]">
               <span className="font-bold ">Ecart</span>,#01, 1st Floor, Concorde
               Silicon Valley Neeladri Road, Doddathogur Village, Begur Hobli,
@@ -32,7 +32,7 @@ const Footer: React.FC = () => {
             </button>
           </div>
         </div>
-        <div className="card-2  md:w-[100%] lg:w-[50%] xl:w-[23%] p-[15px]">
+        <div className=" md:w-[100%] lg:w-[50%] xl:w-[20%] p-[15px]">
           <div>
             <h5 className="text-xl text-blue-600">We Using</h5>
             <h3 className="text-2xl text-blue-400 font-bold p-[15px]">
@@ -48,7 +48,7 @@ const Footer: React.FC = () => {
             </div>
           </div>
         </div>
-        <div className="card-3 sm:w-[100%] md:w-[45%] xl:w-[23%] p-[15px]">
+        <div className="sm:w-[100%] md:w-[45%] xl:w-[20%] p-[15px]">
           <h3 className="text-2xl text-blue-600 mb-[20px]">Pages</h3>
           <div className="flex flex-col ">
             <button className="pb-[5px] text-white hover:text-blue-600 text-left  text-lg active:text-blue-700">
@@ -62,7 +62,7 @@ const Footer: React.FC = () => {
             </button>
           </div>
         </div>
-        <div className="card-4 sm:w-[100%] md:w-[45%] xl:w-[23%] p-[15px] ">
+        {/* <div className="card-4 sm:w-[100%] md:w-[45%] xl:w-[23%] p-[15px] ">
           <h3 className="text-2xl text-blue-600 mb-[20px]"> Quick Links</h3>
           <div className="flex flex-col">
             <button className="text-white hover:text-blue-600  text-lg text-left active:text-blue-700 pb-[5px]">
@@ -96,10 +96,10 @@ const Footer: React.FC = () => {
               FAQS
             </button>
           </div>
-        </div>
+        </div> */}
       </div>
       <div className="indent-56 text-white bg-white-700 flex item-center">
-        <p className="text -lg sm:text-2xl p-[15px]">
+        <p className="text-md text-gray-200 p-[15px]">
           &copy;2022 E-Cart. All Right Reserved
         </p>
       </div>
